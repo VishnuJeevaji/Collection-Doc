7b12522938049051e23af79216aca9fba6cc590c
Vishnu Jeevaji <vishnujeevaji@gmail.com>
2023-02-27 18:26:41 +0530

    Insert Updated

--- list/TestList.java
+++ list/TestList.java
@@ -8,21 +8,27 @@ public class TestList {
 	public static void main(String[] args) {
 	
 		ArrayList list1 = new ArrayList();
+		
 		list1.add("Red");
 		list1.add("Green");
 		list1.add("Orange");
 		list1.add("White");
 		list1.add("Black");
+		list1.add(4, "Pink");
 		
-		TestList.iterateArrayList(list1);
+		//TestList.printArrayList(list1);
+		//TestList.iterateArrayList(list1);
+		TestList.insertArrayList(list1);
 		
 	}
-	//public static void printArrayList(List list) {
-	//	System.out.println(list);
-	//}
-	public static void iterateArrayList(List list) {
-		for(int i=0; i<list.size(); i++) {
-			System.out.println(list.get(i));	
-		}	
+//	public static void printArrayList(List list) {
+//		System.out.println(list);
+//	}
+//	public static void iterateArrayList(List list) {
+//		for(int i=0; i<list.size(); i++) {
+//			System.out.println(list.get(i));	
+//		}
+	public static void insertArrayList(List list) {
+		System.out.println(list);
 	}
 }

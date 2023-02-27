6a36cde7a7c223f2ba99915d9fe1b0be1efd716c
Vishnu Jeevaji <vishnujeevaji@gmail.com>
2023-02-27 18:07:59 +0530

    Iterator Updated

--- list/TestList.java
+++ list/TestList.java
@@ -14,10 +14,15 @@ public class TestList {
 		list1.add("White");
 		list1.add("Black");
 		
-		TestList.printArrayList(list1);
+		TestList.iterateArrayList(list1);
 		
 	}
-	public static void printArrayList(List list) {
-		System.out.println(list);
+	//public static void printArrayList(List list) {
+	//	System.out.println(list);
+	//}
+	public static void iterateArrayList(List list) {
+		for(int i=0; i<list.size(); i++) {
+			System.out.println(list.get(i));	
+		}	
 	}
 }
